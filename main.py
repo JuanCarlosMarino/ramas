7d6a07dd5b99ebc82ed185ce7e68a66f76514b62
JuanCarlosMarino <juan.marino.morantes@gmail.com>
2026-03-06 07:59:06 -0500

    feat: :sparkles: Add menu features
    Add menu features with conditional logic

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 def mostrar_menu():
-    pass
+    mostrar_separador()
+    menu = """Bienvenido
+1. Para agregar fruta
+2. Para ver frutas
+3. Para salir"""
+    print(menu)
+    mostrar_separador()
+    
 
 def mostrar_separador():
-    pass
+    print("*"*15)
 
 def agregar_fruta():
     pass
@@ -11,4 +18,22 @@ def ver_frutas():
     pass
 
 def pedir_opcion():
-    pass
+    opcion = int(input("Ingrese la opcion deseada: "))
+    return opcion
+
+while True:
+    mostrar_menu()
+    try:
+        opc = pedir_opcion()
+    except Exception:
+        print("Hubo un error con su opción ingresada")
+        opc = 3
+    if opc == 1:
+        print("opcion 1")
+    elif opc == 2:
+        print("opcion 2")
+    elif opc == 3:
+        print("Bye bye :)")
+        break
+    else:
+        print("Opción no válida!")
